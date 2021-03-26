351ae7f1743380d0f717db03a07e1f9b96cc0767
arishkaa06 <pokazatskaya02@mail.ru>
2021-03-26 22:44:28 +0300

    to_integer

--- Algebra/Rational.py
+++ Algebra/Rational.py
@@ -12,3 +12,11 @@ class Rational():
 
     def __str__(self):
         return str(self._numerator) + " / " + str(self._denumerator)
+
+    def to_integer(self, n, m):
+        ''' Функция преобразования дробного числа в целое '''
+    # Показацкая Арина
+        self._numerator = n
+        self._denumerator = m
+        if self._denumerator == "1":
+            return self._numerator
